178760c3d477172591c170f99c2bbaac09271379
LuciferPony <e.kudrjashova@gmail.com>
2019-12-03 21:03:30 +0300

    burger button is ready starting sliders

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,16 @@
+import { tns } from 'tiny-slider/src/tiny-slider'
+
 window.onload = function() {
-  console.log('test2')
+  let menu_btn = document.querySelector('.section-header__burger')
+  let header_menu = document.querySelector('.nav__list')
+  menu_btn.onclick = function() {
+    header_menu.classList.toggle('nav__list--active')
+  }
+
+  let slider = tns({
+    container: '.my-slider',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true
+  })
 }
