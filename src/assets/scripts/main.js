ec3595117adf865706f8fa177be5dcc47308b6d5
Eugenia <e.kudrjashova@gmail.com>
2020-01-09 17:51:44 +0300

    working on sliders and js in common

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,7 @@ import { tns } from 'tiny-slider/src/tiny-slider'
 window.onload = function() {
   let menu_btn = document.querySelector('.section-header__burger')
   let header_menu = document.querySelector('.nav__list')
+  let burger_active = document.querySelector('.section')
   menu_btn.onclick = function() {
     header_menu.classList.toggle('nav__list--active')
   }
@@ -13,4 +14,5 @@ window.onload = function() {
     controls: false,
     navContainer: '.main__dotnav'
   })
+
 }
