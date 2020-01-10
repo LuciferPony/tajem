11680814f4939bf9d81e51ff6fe6f81172e61589
Eugenia <e.kudrjashova@gmail.com>
2020-01-10 17:53:56 +0300

    burger is ready, testimonials is ready but needs some work

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,9 +3,10 @@ import { tns } from 'tiny-slider/src/tiny-slider'
 window.onload = function() {
   let menu_btn = document.querySelector('.section-header__burger')
   let header_menu = document.querySelector('.nav__list')
-  let burger_active = document.querySelector('.section')
+  let burger = document.querySelector('.section-header__burger')
   menu_btn.onclick = function() {
     header_menu.classList.toggle('nav__list--active')
+    burger.classList.toggle('active')
   }
 
   let slider = tns({
@@ -13,6 +14,13 @@ window.onload = function() {
     items: 1,
     controls: false,
     navContainer: '.main__dotnav'
+  })
+
+  let slider_testimonials = tns({
+    container: '.section-testimonials__list',
+    items: 1,
+    controls: false,
+    navContainer: '.testimonials__userpic'
   })
 
 }
