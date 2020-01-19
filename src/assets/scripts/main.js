622d166d1e1b17427261d3e481f16d89bd4939cf
LuciferPony <e.kudrjashova@gmail.com>
2020-01-19 22:08:37 +0300

    map is working

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,9 +4,22 @@ window.onload = function() {
   let menu_btn = document.querySelector('.section-header__burger')
   let header_menu = document.querySelector('.nav__list')
   let burger = document.querySelector('.section-header__burger')
+  let map = document.querySelector('.section-map')
+  let map_btn = document.querySelector('.section-map__close')
+  let map_open = document.querySelector('.contacts__address')
+
   menu_btn.onclick = function() {
     header_menu.classList.toggle('nav__list--active')
     burger.classList.toggle('active')
+  }
+
+  map_btn.onclick = function() {
+    map.classList.toggle('modal__active')
+    console.log('closed')
+  }
+
+  map_open.onclick = function() {
+    map.classList.toggle('modal__active')
   }
 
   let slider = tns({
@@ -22,5 +35,4 @@ window.onload = function() {
     controls: false,
     navContainer: '.testimonials__userpic'
   })
-
 }
